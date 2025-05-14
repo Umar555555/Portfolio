f10a4f7d16f9d6af15651f50631b3cc07ec371ad
Umar555555 <umarxon00000000@gmail.com>
2025-05-14 22:01:45 +0500

    jia

--- js/app.js
+++ js/app.js
@@ -12,12 +12,18 @@ let l2 = document.querySelector('.l_2')
 let l3 = document.querySelector('.l_3')
 let l4 = document.querySelector('.l_4')
 let l5 = document.querySelector('.l_5')
+let ll1 = document.querySelector('.ll_1')
+let ll2 = document.querySelector('.ll_2')
+let ll3 = document.querySelector('.ll_3')
+let ll4 = document.querySelector('.ll_4')
+let ll5 = document.querySelector('.ll_5')
 let l7 = document.querySelector('.l_7')
 let l8 = document.querySelector('.l_8')
 let l9 = document.querySelector('.l_9')
 let l10 = document.querySelector('.l_10')
 let l11 = document.querySelector('.l_11')
 let l6 = document.querySelector('.l_6')
+let ll6 = document.querySelector('.ll_6')
 let container = document.querySelector('.container')
 let scrollBtn = document.querySelector('.scroll_btn')
 let contactBtn = document.querySelector('.contact_btn')
@@ -57,6 +63,30 @@ l5.addEventListener('click', ()=>{
 })
 l6.addEventListener('click', ()=>{
     let scw = window.scrollTo(0,2350)
+})
+ll1.addEventListener('click', ()=>{
+    window.scroll(0,0)
+    headerull.classList.add('tops')
+})
+ll2.addEventListener('click', ()=>{
+    window.scroll(0,250)
+    headerull.classList.add('tops')
+})
+ll3.addEventListener('click', ()=>{
+    let scw = window.scrollTo(0,950)
+    headerull.classList.add('tops')
+})
+ll4.addEventListener('click', ()=>{
+    let scw = window.scrollTo(0,1800)
+    headerull.classList.add('tops')
+})
+ll5.addEventListener('click', ()=>{
+    let scw = window.scrollTo(0,2380)
+    headerull.classList.add('tops')
+})
+ll6.addEventListener('click', ()=>{
+    let scw = window.scrollTo(0,2650)
+    headerull.classList.add('tops')
 })
 l7.addEventListener('click', ()=>{
     let scw = window.scrollTo(0,0)
@@ -114,6 +144,17 @@ x.addEventListener('click', ()=>{
 serviceBtn.addEventListener('click', ()=>{
     my.classList.remove('tops')
 })
+
+let burger = document.querySelector('.burger')
+let headerull = document.querySelector('.header_ull')
+let tope = document.querySelector('.tope')
+tope.addEventListener('click', ()=>{
+    headerull.classList.add('tops')
+})
+burger.addEventListener('click', ()=>{
+    headerull.classList.remove('tops')
+})
+
 
 
 
